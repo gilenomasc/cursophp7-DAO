e8da0165982d396ec95f4e11696793fbccd9861d
Gileno Mascarenhas <gilenomasc@hotmail.com>
2019-02-21 05:53:10 -0300

    Criada a classe usuario

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 
 require_once("config.php");
 
+/*
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
+echo json_encode($usuarios); */
+
+$carlos = new Usuario();
+
+$carlos->loadById(2);
+
+echo $carlos;
 
 ?>
